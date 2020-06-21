bb5b09d28932e3350299ae5be0d28f7e13ae4ede
Jae <sprite1345@gmail.com>
2020-06-21 12:23:42 +0900

    Updates

---
+++ precache-manifest.a0462c0525c5b80067498eb895b170ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19365b80ff830c573b2130c50268d706",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "2ce40c0e135efda017fd",
+    "url": "/movie_app/static/css/main.8d4f3e88.chunk.css"
+  },
+  {
+    "revision": "c0071c60e1a78cbbff2e",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ce40c0e135efda017fd",
+    "url": "/movie_app/static/js/main.9201ce76.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
